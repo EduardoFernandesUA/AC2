300d6c25681b4ba0deadf16e0abc8ab6199ca811
EduardoFernandesUA <edu.fernandes@ua.pt>
2022-06-11 18:53:13 +0100

    first

---
+++ adicionais2/Ex1.c
@@ -0,0 +1,66 @@
+#include <detpic32.h>
+
+volatile int n = -1;
+
+int round_div(int a,int b) { return (a + b / 2) / b; }
+
+void refreshDisplays() {
+	static const char display7Scodes[] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67,0x77,0x7C,0x39,0x5E,0x79,0x71};
+    static char sel = 0;
+
+    if( n<0 ) return;
+
+    TRISDbits.TRISD5 = sel;
+    TRISDbits.TRISD6 = sel^1;
+    sel = sel^1;
+
+	LATB = (LATB & 0x80FF) | display7Scodes[ n ] << 8;
+}
+
+int main(void) {
+    /* CONFIGURATIONS */
+
+    // leds<RE3,RE2,RE1,RE0> - pag.162
+    TRISE &= 0xFFF0;
+
+    // displays
+    TRISB &= 0x80FF;
+    TRISD &= 0xFF9F;
+
+    // Timer 2 - 100Hz
+    T2CONbits.TON = 0;
+    T2CONbits.TCKPS = 4;
+    PR2 = round_div(20000000 , (100 * 4)) - 1;
+    TMR2 = 0;
+    IPC2bits.T2IP = 1; // prioridade da interrupção (1 a 6)
+    IFS0bits.T2IF = 0; // limpar pedido de interrupção do temporizador x
+    IEC0bits.T2IE = 1; // ativar pedidos de interrupção do temporizador x
+    T2CONbits.TON = 1;
+
+    /* END OF CONFIGURATIONS */ 
+
+    LATE &= 0xFFF0;
+    TRISDbits.TRISD5 = 0;
+    TRISDbits.TRISD6 = 0;
+
+    while(1){
+        int c = getChar() - '0';
+        if ( c>=0 && c<=3 ) {
+            LATE = (LATE & 0xFFF0) | (0x1 << c);
+            n = c;
+        } else {
+            LATE |= 0x000F;
+            n = 0xFF;
+            resetCoreTimer();
+            while(readCoreTimer()<20000000);
+            LATE &= 0xFFF0;
+            n = -1;
+        }
+    }
+}
+
+void _int_(8) isr_timer_2(void)
+{
+    refreshDisplays();
+    IFS0bits.T2IF = 0; // limpar o pedido de interrupção
+}
